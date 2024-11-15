714cf7ba8e80855e42123c9a4c28dafa5e9a6b19
luis meza <L22550299@chihuahua2.tecnm.mx>
2024-11-14 19:39:03 -0600

    Add files via upload

---
+++ EVA3_7_CURSORES_1.sql
@@ -0,0 +1,28 @@
+delimiter $$
+create procedure dooms_day()
+begin
+	declare emp_id int;
+    declare h_d date;
+    declare j_i varchar(10);
+    declare dep_id int;
+    
+    declare salir int default false;
+	declare datos_emp cursor for
+    select employee_id, hire_date, job_id, department_id from employees; 
+    declare continue handler for not found set salir = true;
+    
+	
+    
+    open datos_emp;
+    
+    empleados: loop
+		fetch datos_emp into emp_id, h_d, j_i, dep_id;
+        if salir then
+			leave empleados;
+		end if;
+        insert into job_history(employee_id, start_date, end_date , JOB_ID, DEPARTMENT_ID)
+        values(emp_id, h_d, curdate(), j_i, dep_id);
+    end loop;
+end$$
+
+delimiter ;
